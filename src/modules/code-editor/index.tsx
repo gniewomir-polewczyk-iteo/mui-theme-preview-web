e0973f898ccc437d7fe30cb5fe9e1871838e96de
Gniewomir Polewczyk <gniewomir.polewczyk@iteo.com>
2023-09-27 11:07:13 +0200

    Update code-editor module

--- src/modules/code-editor/index.tsx
+++ src/modules/code-editor/index.tsx
@@ -1,5 +1,14 @@
+import { LiveEditor, LiveProvider } from 'react-live'
+import { theme } from '../../theme'
+
 const CodeEditor = () => {
-  return <div className="code-editor-module">CodeEditor</div>
+  return (
+    <div className="code-editor-module">
+      <LiveProvider>
+        <LiveEditor code={JSON.stringify(theme, undefined, 2)} />
+      </LiveProvider>
+    </div>
+  )
 }
 
 export default CodeEditor
