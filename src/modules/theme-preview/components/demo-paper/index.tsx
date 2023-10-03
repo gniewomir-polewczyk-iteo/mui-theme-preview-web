8a3666fd27968a6a1d2c3b0f4f60fc413143aa94
Gniewomir Polewczyk <gniewomir.polewczyk@iteo.com>
2023-10-03 12:32:42 +0200

    Update demo-paper style

--- src/modules/theme-preview/components/demo-paper/index.tsx
+++ src/modules/theme-preview/components/demo-paper/index.tsx
@@ -5,27 +5,57 @@ import CodeIcon from '@mui/icons-material/Code'
 const DemoPaper = () => {
   return (
     <div className="demo-paper-wrapper">
-      <Paper>
-        <Box display={'flex'}>
-          <AccessTimeIcon />
-          <Typography variant="caption">March 25th</Typography>
+      <Paper sx={{ bgcolor: 'primary.main', p: 3 }}>
+        <Box sx={{ display: 'flex', alignItems: 'center', mb: 3 }}>
+          <AccessTimeIcon
+            sx={{ color: 'common.white', mr: 1, width: '16px' }}
+          />
+          <Typography sx={{ color: 'common.white', fontSize: '0.75em' }}>
+            March 25th
+          </Typography>
         </Box>
-        <Box>
-          <CodeIcon />
-          <Typography variant="body1">
+        <Box sx={{ mb: 3 }}>
+          <CodeIcon sx={{ color: 'common.white' }} />
+          <Typography sx={{ color: 'common.white', fontSize: '1.15em' }}>
             Customize every button and chip instance primary color
           </Typography>
         </Box>
-        <Box display={'flex'}>
-          <Avatar alt="Remy Sharp" src="/public/images/avatar/1.jpg" />
+        <Box sx={{ display: 'flex', alignItems: 'center', mb: 1 }}>
+          <Avatar
+            alt="Avatar"
+            src="/public/images/avatar/1.jpg"
+            sx={{ border: '2px solid white', mr: 1 }}
+          />
           <div>
-            <Typography variant="body1">Assigned to</Typography>
-            <Typography variant="body2">Michael Scott</Typography>
+            <Typography sx={{ color: 'common.white', fontSize: '0.75em' }}>
+              Assigned to
+            </Typography>
+            <Typography sx={{ color: 'common.white', fontSize: '0.85em' }}>
+              Michael Scott
+            </Typography>
           </div>
         </Box>
-        <Box>
-          <LinearProgress value={60} />
-          <Typography variant="body2">60%</Typography>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <Box sx={{ width: '80%' }}>
+            <LinearProgress
+              variant="determinate"
+              value={60}
+              sx={{
+                '.MuiLinearProgress-bar': { backgroundColor: 'common.white' },
+              }}
+            />
+          </Box>
+          <Typography
+            variant="body2"
+            sx={{
+              color: 'common.white',
+              fontSize: '0.85em',
+              textAlign: 'center',
+              width: '20%',
+            }}
+          >
+            60%
+          </Typography>
         </Box>
       </Paper>
     </div>
