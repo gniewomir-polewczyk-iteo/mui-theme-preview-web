7c2a8c404cca811d674cf26fb0c9827d70cc6a1a
Gniewomir Polewczyk <gniewomir.polewczyk@iteo.com>
2023-10-04 10:45:58 +0200

    Move demo components

--- src/modules/theme-preview/index.tsx
+++ src/modules/theme-preview/index.tsx
@@ -1,13 +1,13 @@
 import { Box, Grid } from '@mui/material'
-import DemoPaper from './components/demo-paper'
-import DemoToggleButtons from './components/demo-toggle-buttons'
-import DemoCalendar from './components/demo-calendar'
-import DemoButtons from './components/demo-buttons'
-import DemoTable from './components/demo-table'
-import DemoMessage from './components/demo-message'
-import DemoChip from './components/demo-chip'
-import DemoTimeline from './components/demo-timeline'
-import DemoRange from './components/demo-range'
+import DemoPaper from '../../components/demo-paper'
+import DemoToggleButtons from '../../components/demo-toggle-buttons'
+import DemoCalendar from '../../components/demo-calendar'
+import DemoButtons from '../../components/demo-buttons'
+import DemoTable from '../../components/demo-table'
+import DemoMessage from '../../components/demo-message'
+import DemoChip from '../../components/demo-chip'
+import DemoTimeline from '../../components/demo-timeline'
+import DemoRange from '../../components/demo-range'
 
 const ThemePreview = () => {
   return (
